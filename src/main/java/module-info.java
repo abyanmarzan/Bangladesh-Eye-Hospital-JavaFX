f8061e37a72c61e540972f48ec6f6ccbc453255f
unknown <abyanmarzan@gmail.com>
2025-11-23 18:33:07 +0600

    edit module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,8 @@ module com.example.bangladesheyehospital {
 
 
     opens com.example.bangladesheyehospital to javafx.fxml;
+    opens com.example.bangladesheyehospital.admin to javafx.fxml, javafx.base;
+    opens com.example.bangladesheyehospital.eyeDoctor to javafx.fxml, javafx.base;
+    opens com.example.bangladesheyehospital.patient to javafx.fxml, javafx.base;
     exports com.example.bangladesheyehospital;
 }
